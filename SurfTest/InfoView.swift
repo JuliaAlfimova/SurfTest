5a2856fd733575e414430f11056232b5678654d1
JuliaAlfimova <juliemoorled@gmail.com>
2023-08-01 13:49:49 +0300

    added info view

--- SurfTest/InfoView.swift
+++ SurfTest/InfoView.swift
@@ -11,6 +11,11 @@ final class InfoView: UIView {
 
     // MARK: - Private properties
 
+    private let userImageViewSizeConst: CGFloat = 120
+    private let bigPadding: CGFloat = 16
+    private let smallPadding: CGFloat = 8
+    private let locationImageName = "mappin.and.ellipse"
+
     private let userImageView = UIImageView()
     private let userNameLabel = UILabel()
     private let userBioLabel = UILabel()
@@ -20,6 +25,7 @@ final class InfoView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
     }
 
     required init?(coder: NSCoder) {
@@ -30,21 +36,100 @@ final class InfoView: UIView {
 
 extension InfoView {
 
+    // MARK: - Public methods
+
+    func configure(image: UIImage?, name: String, bio: String, location: String) {
+        if let image = image {
+            userImageView.image = image
+        }
+        userNameLabel.text = name
+        userBioLabel.text = bio
+        updateAttributedText(with: location, at: userLocationLabel)
+    }
+
     // MARK: - Private methods
 
     private func setupView() {
-        view?.backgroundColor = .systemGray5
+        backgroundColor = .systemGray6
         setupUserImageView()
-        setupUserNameLabel ()
-
+        setupUserNameLabel()
+        setupUserBioLabel()
+        setupUserLocationLabel()
+        updateViewHeight()
     }
 
     private func setupUserImageView() {
+        addSubview(userImageView)
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            userImageView.topAnchor.constraint(equalTo: topAnchor, constant: bigPadding),
+            userImageView.widthAnchor.constraint(equalToConstant: userImageViewSizeConst),
+            userImageView.heightAnchor.constraint(equalToConstant: userImageViewSizeConst)
+        ])
+        userImageView.backgroundColor = .systemGray
+        userImageView.clipsToBounds = true
+        userImageView.layer.cornerRadius = userImageViewSizeConst/2
+    }
+
+    private func setupUserNameLabel() {
+        setupInfoLabel(label: userNameLabel, topAnchor: userImageView.bottomAnchor)
+        userNameLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        userNameLabel.textColor = .label
+        userNameLabel.text = "Name"
+    }
+
+    private func setupUserBioLabel() {
+        setupInfoLabel(label: userBioLabel, topAnchor: userNameLabel.bottomAnchor)
+        userBioLabel.font = .systemFont(ofSize: 14)
+        userBioLabel.textColor = .systemGray
+        userBioLabel.text = "Bio"
+    }
+
+    private func setupUserLocationLabel() {
+        setupInfoLabel(label: userLocationLabel, topAnchor: userBioLabel.bottomAnchor)
+
+        let iconAttachment = NSTextAttachment()
+        iconAttachment.image = UIImage(systemName: locationImageName)?.withTintColor(.systemGray)
+        let attributedString = NSMutableAttributedString()
+        attributedString.append(NSAttributedString(attachment: iconAttachment))
+        attributedString.append(NSAttributedString(string: " "))
+        let textAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 14),
+            .foregroundColor: UIColor.systemGray
+        ]
+        let textAttributedString = NSAttributedString(string: "Location", attributes: textAttributes)
+        attributedString.append(textAttributedString)
+        userLocationLabel.attributedText = attributedString
 
     }
 
-    private func setupUserNameLabel () {
+    private func setupInfoLabel(label: UILabel, topAnchor: NSLayoutYAxisAnchor) {
+        addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: topAnchor, constant: smallPadding),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: bigPadding),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -bigPadding)
+        ])
+        label.textAlignment = .center
+        label.numberOfLines = 0
+    }
 
+    private func updateAttributedText(with newText: String, at label: UILabel) {
+        guard let attributedText = label.attributedText?.mutableCopy() as? NSMutableAttributedString else { return }
+        let range = NSRange(location: 2, length: attributedText.length - 2)
+        attributedText.replaceCharacters(in: range, with: newText)
+        label.attributedText = attributedText
+    }
+
+    private func updateViewHeight() {
+        var totalHeight: CGFloat = bigPadding + userImageViewSizeConst + smallPadding
+        totalHeight += userNameLabel.intrinsicContentSize.height + smallPadding
+        totalHeight += userBioLabel.intrinsicContentSize.height + smallPadding
+        totalHeight += userLocationLabel.intrinsicContentSize.height + bigPadding
+        self.constraints.filter { $0.firstAttribute == .height && $0.relation == .equal }.forEach { $0.constant = totalHeight }
+        heightAnchor.constraint(equalToConstant: totalHeight).isActive = true
     }
 
 }
--- SurfTest/ProfileVC.swift
+++ SurfTest/ProfileVC.swift
@@ -20,12 +20,15 @@ final class ProfileVC: UIViewController {
     private let aboutTitle = "О себе"
     private let aboutUser = "21 год, iOS разработчик, люблю кодить"
     private let userPhotoImageName = "user photo"
-    private let locationImageName = "mappin"
     private let editingImagename = "pencil"
     private let doneImagename = "checkmark.circle"
 
+    private let bigPadding: CGFloat = 16
+    private let smallPadding: CGFloat = 8
+
     private let navigationBar = UINavigationBar()
-    private let infoView = UIView()
+    private let infoView = InfoView()
+    private let scrollView = UIScrollView()
     private let skillsTitleLabel = UILabel()
     private let editingButton = UIButton()
     //skills
@@ -36,7 +39,7 @@ final class ProfileVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setupInterface()
     }
 
     // MARK: - Actions
@@ -50,7 +53,34 @@ extension ProfileVC {
     // MARK: - Private methods
 
     private func setupInterface() {
+        view.backgroundColor = .systemGray6
+        navigationItem.title = navbarTitle
+        setupInfoView()
+        setupScrollView()
+    }
 
+    private func setupInfoView() {
+        let photo = UIImage.init(named: userPhotoImageName)
+        infoView.configure(image: photo, name: userName, bio: userBio, location: userLocation)
+        view.addSubview(infoView)
+        infoView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            infoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: infoView.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        scrollView.backgroundColor = .systemBackground
     }
 
 }
